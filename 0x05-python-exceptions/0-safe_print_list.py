f6f74b448e6aed1b42a7c98ad2d07a9aadc614f4
wang-arshton256 <wangarshton@gmail.com>
2023-08-28 16:19:01 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        my_list = [1, 2, x, g, z, 89]
+       for x in  my_list = [1, 2, x, g, z, 89]
         print(*my_list)
 
     except ListError:
