f1021a06eb2900d1827b3cb7614565011fb90b4e
wang-arshton256 <wangarshton@gmail.com>
2023-08-28 15:30:45 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0)
+    try:
+        my_list = [1,2,x,g,z,89]
+        print(my_list[])
+except 
+    print("Sorry that element does not exist in the list")
