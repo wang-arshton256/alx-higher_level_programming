a7fbebd20ee935c9c71cdad912d07efdc6076f1c
wang-arshton256 <wangarshton@gmail.com>
2023-08-28 17:49:21 -0700

    Safe printing of an integers list

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """Print an interger with "{:d}.format().
+
+    Args:
+        value (int): The interger to print.
+
+    Returns:
+        If a TypeError of ValueErro occurs -false,
+        otherwise - True.
+    """
+    try:
+        print"{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
