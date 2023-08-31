20a0f59aa4941594e7ea6c51f50a8f2a1ef68cd9
wang-arshton256 <wangarshton@gmail.com>
2023-08-30 17:02:40 -0700

    a class Square that defines a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,10 @@ This module contails a class suare which defines a square
 
 
 class Square():
+    """Defines a suare"""
     def __init__(self, size):
-        self._size = size
+        """Sets all the neccesary attributes for the Square object.
+        Args:
+            size (int): the size of one edge of the square.
+        """
+        self.__size = size
