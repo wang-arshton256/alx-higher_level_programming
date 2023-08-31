2d89d5fa89db79257aa5af36d189f8bc509dff47
wang-arshton256 <wangarshton@gmail.com>
2023-08-30 18:01:17 -0700

    Fixed Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -27,6 +27,6 @@ def __init__(self, size=0):
 
     self.__size = size
     if not isinstance(size, int):
-        raise TypeError('size must be an integer')
+        raise TypeError("size must be an integer")
     elif size < 0:
-        raise ValueError('size must be >= 0')
+        raise ValueError("size must be >= 0")
