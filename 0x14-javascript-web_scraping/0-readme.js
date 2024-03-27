13ef9f528b7b45d354cf8e4c69c4179807131ada
Wangutusi Arshton <wangarshton@gmail.com>
2024-03-27 05:30:17 +0300

    fixed errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function(err, data) {
-    if (err) {
-        console.log(err);
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (error, content) => {
+    if (error) {
+        console.log(error);
     } else {
-        console.log(data);
+        console.log(content);
     }
 });
